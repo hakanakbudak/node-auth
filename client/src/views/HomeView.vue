55f919db1e29dcc7f44887f17373700f2c924e8f
[hakanakbudak] <[hakanakbudak06@gmail.com]>
2023-03-23 14:18:01 +0300

    first commit

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+  <h1 class="title">Json Web Token - Login</h1>
+  <h2 class="subtitle">
+    Bu denemede json web token kullanarak<strong>sections</strong>, like the one you're currently reading.
+  </h2>
+</section>
+
+</template>
+
+<script>
+
+
+</script>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <LoginForm :post="post" :submitLoginForm="createUser" />
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+export default {
+    components: {
+        LoginForm,
+    },
+
+    
+    
+
+    
+}
+
+
+
+</script>
+
+
+<style></style>
